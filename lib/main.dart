40b2b83f1baa76a24ae8f8b90aed91de395340a7
Adham Lasheen <alasheen0611@gmail.com>
2025-05-10 18:58:21 +0300

    Refactor Home widget UI: update menu items and apply Google Fonts styling

--- lib/main.dart
+++ lib/main.dart
@@ -30,26 +30,40 @@ class Home extends StatelessWidget {
               width: 250,
               height: 500,
               child: Container(
+                decoration: BoxDecoration(
                 color: Colors.black87,
+                 borderRadius: 
+                 BorderRadius.circular(8),
+                 ),
                 child: Column(
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Text('Menu', style:GoogleFonts
-                          .fascinateInline()
+                          .lora()
                           .copyWith(color: Colors.white, fontSize: 35)),
                     ),
         
                     const Divider(
+                      color: Colors.white,
                       thickness: 2,
                     ),
         
                     ListTile(
-                      title: Text('Home'),
-                      leading: const Icon(Icons.home),
-                      subtitle: Text('Where you sleep'),
-                    )
-                    
+                      title: Text('resipes'),
+                      leading: const Icon(Icons.menu_book_outlined),
+                    ),
+                    ListTile(
+                      title: Text('ingredients'),
+                      leading: const Icon(Icons.egg),
+                      
+                    ),ListTile(
+                      title: Text('saved recipes'),
+                      leading: const Icon(Icons.offline_pin)
+                    ),
+                    ListTile(
+                      title: Text('help'),
+                      leading: const Icon(Icons.help),)
                   ],
                 ),
               ),
