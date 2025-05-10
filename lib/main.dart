9b8f888a49967ff80ccffdb545cf158b46b596b2
Adham Lasheen <alasheen0611@gmail.com>
2025-05-10 17:23:15 +0300

    I did the black and white part

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(title: 'Flutter Demo', home: const Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Row(
+        children: [
+          SizedBox(width: 250, child: Container(color:  Colors.black87)),
+          Expanded(
+            child: Container(
+              color: Colors.black12,
+              child: Center(child: Text('Hellow')),
+            ),
+          ),
+        ],
       ),
     );
   }
